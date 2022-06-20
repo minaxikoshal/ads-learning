da6ed1b08aa85faf0019145ad9d4a3d76ba75d2c
minaxikoshal <aahana7317@gmail.com>
2022-06-20 22:46:38 +0530

    Updated

--- React/myproject/src/App.js
+++ React/myproject/src/App.js
@@ -38,7 +38,7 @@ const App=()=> {
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <ExpensesFilter />
-      {/* <Expenses items={expenses} /> */}
+      <Expenses items={expenses} />
       {/* <ExpenseItems title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItems>
       <ExpenseItems title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItems>
       <ExpenseItems title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItems>
--- React/myproject/src/components/expenses/Expenses.js
+++ React/myproject/src/components/expenses/Expenses.js
@@ -6,20 +6,17 @@ import './Expenses.css' ;
 
 const Expenses=(props)=>{
   // const [filteredYear, setFilterYear]=useState('2021');
-
   // const filterChangeHandler=selectedYear=>{
   //   setFilteredYear(selectedYear);
   // }
-
-
 return(
         <Card className="expenses">
-         {/* <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} /> */}
-        {/*{props.items.map(expense=> <ExpenseItems title={expense.title} amount={expense.amount} date={expense.date} />)} */}
-      <ExpenseItems title={props[0].title} amount={props[0].amount} date={props[0].date}></ExpenseItems>
-      <ExpenseItems title={props[1].title} amount={props[1].amount} date={props[1].date}></ExpenseItems>
-      <ExpenseItems title={props[2].title} amount={props[2].amount} date={props[2].date}></ExpenseItems>
-      <ExpenseItems title={props[3].title} amount={props[3].amount} date={props[3].date}></ExpenseItems>
+         {/* <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {props.items.map(expense=> <ExpenseItems title={expense.title} amount={expense.amount} date={expense.date} />)} */}
+      <ExpenseItems title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItems>
+      <ExpenseItems title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItems>
+      <ExpenseItems title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItems>
+      <ExpenseItems title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItems>
 
         </Card>
     );
