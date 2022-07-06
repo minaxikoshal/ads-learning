1004e7b0bd705953dfdfe96884ad442fb95ec271
minaxikoshal <aahana7317@gmail.com>
2022-07-06 12:44:23 +0530

    Updated

---
+++ React/04-starting-project/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState, useCallback} from 'react';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph]=useState(false);
+  const [allowToggle, setAllowToggle]=useState(false);
+
+  const toggleParagraphHandler=useCallback(()=>{
+    setShowParagraph(prevshowParagraph=> !prevshowParagraph)
+  }, []);
+
+  const allowToggleHandler=()=>{
+    setAllowToggle(true);
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
